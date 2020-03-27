581cb572642ca1fc686f193f10af68cfa35daa27
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-27 09:49:02 +0200

    added OutputResponse

---
+++ Thandizo.DataModels/General/OutputResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Thandizo.DataModels.General
+{
+    public class OutputResponse
+    {
+        public bool IsErrorOccured { get; set; }
+        public string Description { get; set; }
+    }
+}
