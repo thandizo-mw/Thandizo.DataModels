d324f79665a5d5fb01ed5278d8a0d179ec2bf87d
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-27 10:14:05 +0200

    added result property to OutputResponse

--- Thandizo.DataModels/General/OutputMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Thandizo.DataModels.General
-{
-    public class OutputMessage
-    {
-        public bool IsErrorOccured { get; set; }
-        public string Description { get; set; }
-    }
-}
---
+++ Thandizo.DataModels/General/OutputResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Thandizo.DataModels.General
+{
+    public class OutputResponse
+    {
+        public bool IsErrorOccured { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
