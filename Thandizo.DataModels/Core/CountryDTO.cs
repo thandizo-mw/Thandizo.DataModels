62a6b7c12e04062f011368094ffa82b44c12e454
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-29 09:34:04 +0200

    added CountryDTO and NationalityDTO under Core namespace

---
+++ Thandizo.DataModels/Core/CountryDTO.cs
@@ -0,0 +1,10 @@
+﻿using Thandizo.DataModels.General;
+
+namespace Thandizo.DataModels.Core
+{
+    public class CountryDTO : BaseRowDTO
+    {
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+    }
+}
---
+++ Thandizo.DataModels/Core/NationalityDTO.cs
@@ -0,0 +1,10 @@
+﻿using Thandizo.DataModels.General;
+
+namespace Thandizo.DataModels.Core
+{
+    public class NationalityDTO : BaseRowDTO
+    {
+        public string NationalityCode { get; set; }
+        public string NationalityName { get; set; }
+    }
+}
