07c8f1f0e5ace875a53b9a9045e0d1482919a162
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-29 09:35:15 +0200

    added DataCreatorDTO and changed BaseRowDTO under General namespace

--- Thandizo.DataModels/General/BaseRowDTO.cs
+++ Thandizo.DataModels/General/BaseRowDTO.cs
@@ -2,10 +2,8 @@
 
 namespace Thandizo.DataModels.General
 {
-    public abstract class BaseRowDTO
-    {
-        public DateTime DateCreated { get; set; }
-        public string CreatedBy { get; set; }
+    public abstract class BaseRowDTO : DataCreatorDTO
+    {        
         public DateTime? DateModified { get; set; }
         public string ModifiedBy { get; set; }
         public string RowAction { get; set; }
---
+++ Thandizo.DataModels/General/DataCreatorDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Thandizo.DataModels.General
+{
+    public abstract class DataCreatorDTO
+    {
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
