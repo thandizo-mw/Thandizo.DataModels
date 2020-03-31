9ac81bd20a0968507664c7155e3e2440c35b22a4
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-31 15:06:41 +0200

    added data annotations in CountryDTO

--- Thandizo.DataModels/Core/CountryDTO.cs
+++ Thandizo.DataModels/Core/CountryDTO.cs
@@ -1,10 +1,17 @@
-﻿using Thandizo.DataModels.General;
+﻿using System.ComponentModel.DataAnnotations;
+using Thandizo.DataModels.General;
 
 namespace Thandizo.DataModels.Core
 {
     public class CountryDTO : BaseRowDTO
     {
+        [Required(ErrorMessage = "Country Code cannot be blank")]
+        [MinLength(2, ErrorMessage ="Minimum number of characters for Country Code is 2")]
+        [MaxLength(3, ErrorMessage = "Maximum number of characters for Country Code is 3")]
         public string CountryCode { get; set; }
+
+        [Required(ErrorMessage = "Country Name cannot be blank")]
+        [MaxLength(40, ErrorMessage = "Maximum number of characters for Country Name is 40")]
         public string CountryName { get; set; }
     }
 }
