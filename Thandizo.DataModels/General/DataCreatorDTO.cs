e71151bce3f3df947d1a4440abe21dd78ab1492a
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-31 15:05:49 +0200

    added data annotations to DataCreatorDTO

--- Thandizo.DataModels/General/DataCreatorDTO.cs
+++ Thandizo.DataModels/General/DataCreatorDTO.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Thandizo.DataModels.General
 {
     public abstract class DataCreatorDTO
     {
         public DateTime DateCreated { get; set; }
+
+        [Required(ErrorMessage = "Created By cannot be blank")]
+        [MaxLength(40, ErrorMessage = "Maximum number of characters for Created By is 40")]
         public string CreatedBy { get; set; }
     }
 }
