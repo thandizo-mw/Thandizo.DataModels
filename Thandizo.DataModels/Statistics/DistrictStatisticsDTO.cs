b16f04c0cd2d225c09b7dc624d1492eab12fdbcf
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-04-02 15:57:08 +0200

    removed data annotations, they are not required in DistrictStatisticsDTO and NationalStatisticsDTO

--- Thandizo.DataModels/Statistics/DistrictStatisticsDTO.cs
+++ Thandizo.DataModels/Statistics/DistrictStatisticsDTO.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Thandizo.DataModels.Statistics
+﻿namespace Thandizo.DataModels.Statistics
 {
     public class DistrictStatisticsDTO : RegionalStatisticsDTO
     {
-        [Required(ErrorMessage = "District Name cannot be blank")]
-        [MaxLength(50, ErrorMessage = "Maximum length for District Name is 50")]
         public string DistrictName { get; set; }
     }
 }
--- Thandizo.DataModels/Statistics/NationalStatisticsDTO.cs
+++ Thandizo.DataModels/Statistics/NationalStatisticsDTO.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Thandizo.DataModels.Statistics
+﻿namespace Thandizo.DataModels.Statistics
 {
     public class NationalStatisticsDTO
     {
-        [Required(ErrorMessage = "Patient Status Name cannot be blank")]
-        [MaxLength(50, ErrorMessage = "Maximum length for Patient Status Name is 50")]
         public string PatientStatusName { get; set; }
         public int TotalNumberOfPatients { get; set; }
     }
