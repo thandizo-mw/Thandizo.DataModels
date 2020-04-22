6fddd5eb427ef90c2394a0d9f4d636cf93b0745f
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-22 15:08:05 +0200

    Removed JSON aliases on UssdOutboundDTO object

--- Thandizo.DataModels/Ussd/UssdOutboundDTO.cs
+++ Thandizo.DataModels/Ussd/UssdOutboundDTO.cs
@@ -1,16 +1,10 @@
-﻿using Newtonsoft.Json;
-
-namespace Thandizo.DataModels.Ussd
+﻿namespace Thandizo.DataModels.Ussd
 {
     public class UssdOutboundDTO
     {
-        [JsonProperty("m")]
         public string Msisdn { get; set; }
-        [JsonProperty("r")]
         public string Response { get; set; }
-        [JsonProperty("s")]
         public string sessionid { get; set; }
-        [JsonProperty("t")]
         public string Type { get; set; }
     }
 }
