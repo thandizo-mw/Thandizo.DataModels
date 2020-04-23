192b08ac7b52656f3dbe23791778281909eddeaa
Noble Hara <noble.hara@angledimension.com>
2020-04-23 10:03:44 +0200

    Added full name to UserDTO

--- Thandizo.DataModels/Identity/DataTransfer/UserDTO.cs
+++ Thandizo.DataModels/Identity/DataTransfer/UserDTO.cs
@@ -8,5 +8,9 @@ namespace Thandizo.DataModels.Identity.DataTransfer
         [MinLength(10, ErrorMessage = "Minimum number of characters for phone number is 10")]
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
+        [Required(ErrorMessage = "Full name cannot be blank")]
+        [MinLength(4, ErrorMessage = "Minimum number of characters for full name is 4")]
+        public string FullName { get; set; }
+
     }
 }
