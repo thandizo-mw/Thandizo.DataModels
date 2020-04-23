6fbd910a42371ebb67e9abb93389229ea1a0cae7
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-23 12:56:27 +0200

    Removed .NET Core JSON serializer attributes

--- Thandizo.DataModels/Ussd/UssdInboundDTO.cs
+++ Thandizo.DataModels/Ussd/UssdInboundDTO.cs
@@ -1,16 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Runtime.Serialization;
 
 namespace Thandizo.DataModels.Ussd
 {
     public class UssdInboundDTO
     {
-        [JsonPropertyName("m")]
+        [DataMember(Name = "m")]
         public string Msisdn { get; set; }
-        [JsonPropertyName("b")]
+        [DataMember(Name = "b")]
         public string Body { get; set; }
-        [JsonPropertyName("s")]
+        [DataMember(Name = "s")]
         public string Sessionid { get; set; }
-        [JsonPropertyName("t")]
+        [DataMember(Name = "t")]
         public string Type { get; set; }
     }
 }
