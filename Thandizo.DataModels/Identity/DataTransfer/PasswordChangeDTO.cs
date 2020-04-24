11df05f46309c69bc464f7a358a17476f8e2d345
Noble Hara <noble.hara@angledimension.com>
2020-04-24 11:23:41 +0200

    Remove required on Username

--- Thandizo.DataModels/Identity/DataTransfer/PasswordChangeDTO.cs
+++ Thandizo.DataModels/Identity/DataTransfer/PasswordChangeDTO.cs
@@ -4,12 +4,9 @@ namespace Thandizo.DataModels.Identity.DataTransfer
 {
     public class PasswordChangeDTO
     {
-        [Required(ErrorMessage = "Phone number cannot be blank")]
-        [MinLength(10, ErrorMessage = "Minimum number of characters for phone number is 10")]
         public string Username { get; set; }
-
         public string CurrentPassword { get; set; }
-
+        
         [Required(ErrorMessage = "Current password cannot be blank")]
         [MinLength(6, ErrorMessage = "Minimum number of characters for the new password is 6")]
         public string NewPassword { get; set; }
