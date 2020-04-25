2cd2bfc865cc471bc6a6b3705964c50e7d45b295
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-04-25 16:20:11 +0200

    added DhisTrackedEntity and DhisTrackedEntityAttribute for supporting DHIS2 system integration

---
+++ Thandizo.DataModels/Integrations/DhisTrackedEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Thandizo.DataModels.Integrations
+{
+    public class DhisTrackedEntity
+    {
+        public string TrackedEntity { get; set; }
+        public string OrgUnit { get; set; }
+        public IEnumerable<DhisTrackedEntityAttribute> Attributes { get; set; }
+    }
+}
