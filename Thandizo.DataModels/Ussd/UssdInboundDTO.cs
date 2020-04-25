887796c3b4310afc64a78d7fc651b4aae0cd6f11
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-25 12:09:42 +0200

    Removed data annotation on UssdInboundDTO

--- Thandizo.DataModels/Ussd/UssdInboundDTO.cs
+++ Thandizo.DataModels/Ussd/UssdInboundDTO.cs
@@ -4,13 +4,9 @@ namespace Thandizo.DataModels.Ussd
 {
     public class UssdInboundDTO
     {
-        [DataMember(Name = "m")]
         public string Msisdn { get; set; }
-        [DataMember(Name = "b")]
         public string Body { get; set; }
-        [DataMember(Name = "s")]
         public string Sessionid { get; set; }
-        [DataMember(Name = "t")]
         public string Type { get; set; }
     }
 }
