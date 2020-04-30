e3872fa6802c20468f7ab8645ab3b0eaea1d42e9
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-30 12:29:08 +0200

    Added SesstionType enum

---
+++ Thandizo.DataModels/Ussd/SessionType.cs
@@ -0,0 +1,18 @@
+﻿namespace Thandizo.DataModels.Ussd
+{
+    public enum SessionType
+    {
+        /// <summary>
+        /// New request from mobile device 
+        /// </summary>
+        New = 1,
+        /// <summary>
+        /// Continuation of previous session
+        /// </summary>
+        Continious = 2,
+        /// <summary>
+        /// End of session
+        /// </summary>
+        End = 3
+    }
+}
