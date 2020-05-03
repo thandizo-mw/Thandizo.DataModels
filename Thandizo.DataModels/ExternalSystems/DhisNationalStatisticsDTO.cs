1e84fbe4c803cf908e2952c884a1b61fee97b9a5
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-03 10:25:52 +0200

    added WorldStatisticsDTO and DhisNationalStatisticsDTO

---
+++ Thandizo.DataModels/ExternalSystems/DhisNationalStatisticsDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Thandizo.DataModels.ExternalSystems
+{
+    public class DhisNationalStatisticsDTO
+    {
+        public int NumberOfSuspectedCases { get; set; }
+        public int NumberOfConfirmedCases { get; set; }
+        public int NumberOfConfirmedDeaths { get; set; }
+        public int NumberOfRecoveredPatients { get; set; }
+        public int NumberOfActiveCases { get; set; }
+        public int NumberOfReceivedSamples { get; set; }
+        public int NumberOfTestedSamples { get; set; }
+    }
+}
