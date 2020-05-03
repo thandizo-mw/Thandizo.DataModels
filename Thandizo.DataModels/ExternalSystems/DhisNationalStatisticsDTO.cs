a93b1ddc45517e34e319719a9096717c209276b7
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-03 10:35:49 +0200

    changed NumberOfActiveCases property to derived one

--- Thandizo.DataModels/ExternalSystems/DhisNationalStatisticsDTO.cs
+++ Thandizo.DataModels/ExternalSystems/DhisNationalStatisticsDTO.cs
@@ -6,7 +6,13 @@
         public int NumberOfConfirmedCases { get; set; }
         public int NumberOfConfirmedDeaths { get; set; }
         public int NumberOfRecoveredPatients { get; set; }
-        public int NumberOfActiveCases { get; set; }
+        public int NumberOfActiveCases
+        {
+            get
+            {
+                return NumberOfConfirmedCases - (NumberOfConfirmedDeaths + NumberOfRecoveredPatients);
+            }
+        }
         public int NumberOfReceivedSamples { get; set; }
         public int NumberOfTestedSamples { get; set; }
     }
