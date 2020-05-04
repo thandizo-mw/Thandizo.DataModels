a63a3776d0b10afa71ed3300bbb960084d67734e
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-04 15:09:31 +0200

    Added NotificationsChannelDTO class

---
+++ Thandizo.DataModels/Notifications/NotificationChannelDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Thandizo.DataModels.General;
+
+namespace Thandizo.DataModels.Notifications
+{
+    public class NotificationChannelDTO : BaseRowDTO
+    {
+        public int ChannelId { get; set; }
+
+        [Required(ErrorMessage="Channel Name cannot be blank")]
+        [MaxLength(40, ErrorMessage="Maximum length for Channel Name is 40")]
+        [Display(Name = "Name")]
+        public string ChannelName { get; set; }
+    }
+}
