a9280d399cba4065e4495552f4a49148e49acc09
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-04 15:08:56 +0200

    Added BulkNotificationsDTO class

---
+++ Thandizo.DataModels/Notifications/BulkNotificationDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Thandizo.DataModels.General;
+
+namespace Thandizo.DataModels.Notifications
+{
+    public class BulkNotificationDTO : BaseRowDTO
+    {
+        public long NotificationId { get; set; }
+
+        [Display(Name = "Message")]
+        [Required(ErrorMessage = "Message cannot be blank")]
+        [MaxLength(1000, ErrorMessage = "Maximum length for Message is 1000")]
+        public string Message { get; set; }
+
+        [Display(Name = "Send Date")]
+        [Required(ErrorMessage = "Send Date cannot be blank")]
+        public DateTime SendDate { get; set; }
+
+
+
+    }
+}
