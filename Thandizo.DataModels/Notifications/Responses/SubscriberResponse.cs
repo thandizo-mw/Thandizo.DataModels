bc5dd470939e9785bf5bd89f9f57934ad12bf9bb
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-05 12:22:31 +0200

    Added SubscriberResponse class

---
+++ Thandizo.DataModels/Notifications/Responses/SubscriberResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thandizo.DataModels.Notifications.Responses
+{
+    public class SubscriberResponse : SubscriberDTO
+    {
+        public string ChannelName { get; set; }
+    }
+}
