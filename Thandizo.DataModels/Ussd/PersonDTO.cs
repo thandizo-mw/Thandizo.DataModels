e7a499d595922521c023347fcffa4b4351c8190d
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-07 07:20:48 +0200

    added PersonDTO and SharedListItems classes

---
+++ Thandizo.DataModels/Ussd/PersonDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Thandizo.DataModels.Ussd
+{
+    public  class PersonDTO
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string OtherNames { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ Thandizo.DataModels/Ussd/SharedListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Thandizo.DataModels.Ussd
+{
+    public class SharedListItem
+    {
+        public int ItemIndex { get; set; }
+        public object ItemCode { get; set; }
+        public string ItemDescription { get; set; }
+    }
+}
