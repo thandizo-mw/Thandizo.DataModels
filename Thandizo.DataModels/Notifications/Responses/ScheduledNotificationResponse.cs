d52a0b0a1a8aae96969a103e654540251677049c
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-10 17:42:23 +0200

    Added ScheduledNotificationResponse class

--- Thandizo.DataModels/Notifications/Responses/ScheduledNotificationResponse.cs
+++ Thandizo.DataModels/Notifications/Responses/ScheduledNotificationResponse.cs
@@ -10,5 +10,19 @@ namespace Thandizo.DataModels.Notifications.Responses
         public string ChanneldName { get; set; }
         public string RuleName { get; set; }
         public string PatientName { get; set; }
+        public string IntervalName
+        {
+            get
+            {
+                return Interval switch
+                {
+                    "O" => "One Time",
+                    "D" => "Daily",
+                    "W" => "Weekly",
+                    "M" => "Monthly",
+                    _ => "None",
+                };
+            }
+        }
     }
 }
