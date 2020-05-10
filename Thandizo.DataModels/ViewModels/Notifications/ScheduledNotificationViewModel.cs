8af1add08b197082eb442a36a0ef4b9936ebf0e1
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-10 17:43:02 +0200

    Added ScheduledNotificationViewModel class

---
+++ Thandizo.DataModels/ViewModels/Notifications/ScheduledNotificationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Thandizo.DataModels.Notifications;
+
+namespace Thandizo.DataModels.ViewModels.Notifications
+{
+    public class ScheduledNotificationViewModel
+    {
+        public ScheduledNotificationDTO ScheduledNotification { get; set; }
+        public IEnumerable<NotificationChannelDTO> NotificationChannels { get; set; }
+        public IEnumerable<ScheduledNotificationEscalationRuleDTO> EscalationRules { get; set; }
+        public IEnumerable<NotificationTemplateDTO> NotificationTemplates { get; set; }
+    }
+}
