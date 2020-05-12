109f662d4205ee93ff6548667ccaf7551c129d17
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-12 11:23:07 +0200

    Removed PhoneNumber property and added Subscription Date and RecipientAddress properties to SubscriberDTO class

--- Thandizo.DataModels/Notifications/SubscriberDTO.cs
+++ Thandizo.DataModels/Notifications/SubscriberDTO.cs
@@ -16,13 +16,18 @@ namespace Thandizo.DataModels.Notifications
         public int ChannelId { get; set; }
 
 
-        [Display(Name = "Phone Number")]
-        [MaxLength(20, ErrorMessage = "Maximum length for Phone Number is 20")]
-        public string PhoneNumber { get; set; }
+        [Display(Name = "Recipient Address")]
+        [Required(ErrorMessage = "Recipient Address cannot be blank")]
+        [MaxLength(60, ErrorMessage = "Maximum length for Recipient Address is 60")]
+        public string RecipientAddress { get; set; }
 
         [Display(Name = "Is Registered Patient")]
         [Required(ErrorMessage = "Is Registered cannot be blank")]
         [Range(typeof(bool), "False", "True", ErrorMessage = "Is Registered Patient has to either be true or false")]
         public bool IsRegisteredPatient { get; set; }
+
+        [Display(Name = "Subscription Date")]
+        [Required(ErrorMessage = "Subscription Date cannot be blank")]
+        public DateTime SubscriptionDate { get; set; }
     }
 }
