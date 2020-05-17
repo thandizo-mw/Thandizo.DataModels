107938d36171750c59fc8a93e21ea3784dd29557
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-17 16:20:22 +0200

    added DhisPatientDailyStatusRequest and IDhisPatientDailyStatusRequest under Contracts

---
+++ Thandizo.DataModels/Contracts/IDhisPatientDailyStatusRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Thandizo.DataModels.Patients;
+
+namespace Thandizo.DataModels.Contracts
+{
+    public interface IDhisPatientDailyStatusRequest
+    {
+        IEnumerable<PatientDailyStatusDTO> Statuses { get; }
+    }
+}
