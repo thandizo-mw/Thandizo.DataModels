08f006355a56da1135b21f842a02fb6e4928f6f3
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-19 10:52:21 +0200

    Added BulkNotificationRequestViewModel class

---
+++ Thandizo.DataModels/ViewModels/Notifications/BulkNotificationRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Thandizo.DataModels.Notifications;
+using Thandizo.DataModels.Notifications.Requests;
+
+namespace Thandizo.DataModels.ViewModels.Notifications
+{
+    public class BulkNotificationRequestViewModel
+    {
+        public BulkNotificationRequest BulkNotificationRequest { get; set; }
+
+        public IEnumerable<NotificationChannelDTO> Channels { get; set; }
+        
+    }
+}
