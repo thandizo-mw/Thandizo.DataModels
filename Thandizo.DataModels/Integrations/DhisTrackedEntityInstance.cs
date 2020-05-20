2816ff9bbe17db8f6e007bb6865f309262e2faab
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-20 18:27:38 +0200

    added DhisTrackedEntityInstance

---
+++ Thandizo.DataModels/Integrations/DhisTrackedEntityInstance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Thandizo.DataModels.Integrations
+{
+    public class DhisTrackedEntityInstance
+    {
+        public string Program { get; set; }
+        public string ProgramStage { get; set; }
+        public string OrgUnit { get; set; }
+        public string TrackedEntityInstance { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Status { get; set; }
+        public DateTime CompletedDate { get; set; }
+        public IEnumerable<DhisDataValue> DataValues { get; set; }
+    }
+}
