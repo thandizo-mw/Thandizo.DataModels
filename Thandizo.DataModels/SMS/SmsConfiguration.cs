5384ad3d7ec1fa2d38e804659e74c9aa65f1e960
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-21 16:02:40 +0200

    added SmsConfiguration

---
+++ Thandizo.DataModels/SMS/SmsConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thandizo.DataModels.SMS
+{
+    public class SmsConfiguration
+    {
+        public string SmsSender { get; set; }
+        public string BaseUrl { get; set; }
+        public string RapidProUserName { get; set; }
+        public string RapidProPassword { get; set; }
+    }
+}
