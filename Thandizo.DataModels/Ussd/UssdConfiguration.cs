67856893f3de1f1c2ecb33f71f8cac9ff17145fd
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-23 15:46:59 +0200

    added IdentityServerApiUrl property in UssdConfiguration

--- Thandizo.DataModels/Ussd/UssdConfiguration.cs
+++ Thandizo.DataModels/Ussd/UssdConfiguration.cs
@@ -8,5 +8,6 @@
         public string CoreApiUrl { get; set; }
         public string VirusTrackerApiUrl { get; set; }
         public string DhisApiUrl { get; set; }
+        public string IdentityServerApiUrl { get; set; }
     }
 }
