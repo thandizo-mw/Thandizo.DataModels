5c11cd9d66973003d1615ded6f539941bd00d3ae
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-06-05 18:19:31 +0200

    Added InboundMessage object for Africa's talking gateway

---
+++ Thandizo.DataModels/Integrations/AT/ATInboundMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Thandizo.DataModels.Integrations.AT
+{
+    public class ATInboundMessage
+    {
+        public string SessionId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Text { get; set; }
+        public string ServiceCode { get; set; }
+    }
+}
