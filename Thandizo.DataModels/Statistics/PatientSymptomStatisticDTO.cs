1f337fefe5456b765db3feafbc77bea56b2236d2
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-07-10 17:15:38 +0200

    Added PatientSymptomStatisticsDTO class

---
+++ Thandizo.DataModels/Statistics/PatientSymptomStatisticDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thandizo.DataModels.Statistics
+{
+    public class PatientSymptomStatisticDTO
+    {
+        public int TotalNumberOfReports { get; set; }
+
+        public string SymptomName { get; set; }
+    }
+}
